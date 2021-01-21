aa9e24bc30b6b7964de252f8496a13269242e3d2
Georg Dangl <georg@dangl.me>
2021-01-21 15:34:28 +0100

    Add feature to export Revit SectionBox as a set of 6 clipping planes

--- src/OpenProject.Revit/Data/RevitView.cs
+++ src/OpenProject.Revit/Data/RevitView.cs
@@ -1,8 +1,9 @@
-using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using OpenProject.Shared;
 using OpenProject.Shared.ViewModels.Bcf;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OpenProject.Revit.Data
@@ -127,6 +128,7 @@ namespace OpenProject.Revit.Data
         }
 
         SetViewpointComponents(bcfViewpoint, doc, uidoc);
+        SetViewpointClippingPlanes(bcfViewpoint, uidoc);
 
         return bcfViewpoint;
       }
@@ -156,8 +158,6 @@ namespace OpenProject.Revit.Data
          ;//would need to check how much this is affecting performance
 
       var selectedElems = uidoc.Selection.GetElementIds();
-
-      //TODO: create clipping planes
 
       bcfViewpoint.Components = new iabi.BCF.APIObjects.V21.Components();
       if (hiddenElems.Count > visibleElems.Count)
@@ -191,7 +191,7 @@ namespace OpenProject.Revit.Data
 
       if (selectedElems.Any())
       {
-        bcfViewpoint.Components.Selection = new System.Collections.Generic.List<iabi.BCF.APIObjects.V21.Component>();
+        bcfViewpoint.Components.Selection = new List<iabi.BCF.APIObjects.V21.Component>();
         foreach (var selectedComponent in selectedElems)
         {
           bcfViewpoint.Components.Selection.Add(new iabi.BCF.APIObjects.V21.Component
@@ -202,6 +202,78 @@ namespace OpenProject.Revit.Data
           });
         }
       }
+    }
+
+    private static void SetViewpointClippingPlanes(BcfViewpointViewModel bcfViewpoint,
+      UIDocument uidoc)
+    {
+      // There are actually no clipping planes in a 3d view in revit, rather it's a
+      // section box, meaning a set of 6 clipping planes that wrap the visible part
+      // of a model inside a cuboid shape.
+      if (!(uidoc.ActiveView is View3D view3d) || !view3d.IsSectionBoxActive)
+      {
+        return;
+      }
+
+      bcfViewpoint.Viewpoint.Clipping_planes = new List<iabi.BCF.APIObjects.V21.Clipping_plane>();
+      var revitSectionBox = view3d.GetSectionBox();
+
+      // The Min and Max represent two corners of the section box on opposite
+      // sides, so they're describing a cuboid 3D geometry, which we can now
+      // use to derive the six actual planes from
+      var transformedMin = revitSectionBox.Transform.OfPoint(revitSectionBox.Min);
+      var transformedMax = revitSectionBox.Transform.OfPoint(revitSectionBox.Max);
+
+      // A clipping plane is defined as a point and a direction, so we now
+      // can use each point (Min and Max) three times, each with one direction in
+      // X, Y and Z pointing outwards from the center
+      // The vectors for the Min point should point away from the center (negative)
+      // and the vectors for the Max point should be positive
+      bcfViewpoint.Viewpoint.Clipping_planes = new[]
+      {
+        new {
+          Location = transformedMin,
+          Direction = revitSectionBox.Transform.OfVector(new XYZ(-1,0,0))
+        },
+        new {
+          Location = transformedMin,
+          Direction = revitSectionBox.Transform.OfVector(new XYZ(0,-1,0))
+        },
+        new {
+          Location = transformedMin,
+          Direction = revitSectionBox.Transform.OfVector(new XYZ(0,0,-1))
+        },
+        new {
+          Location = transformedMax,
+          Direction = revitSectionBox.Transform.OfVector(new XYZ(1,0,0))
+        },
+        new {
+          Location = transformedMax,
+          Direction = revitSectionBox.Transform.OfVector(new XYZ(0,1,0))
+        },
+        new {
+          Location = transformedMax,
+          Direction = revitSectionBox.Transform.OfVector(new XYZ(0,0,1))
+        }
+      }
+      .Select(cp =>
+        new iabi.BCF.APIObjects.V21.Clipping_plane
+        {
+          Location = new iabi.BCF.APIObjects.V21.Location
+          {
+            X = Convert.ToSingle(cp.Location.X.ToMeters()),
+            Y = Convert.ToSingle(cp.Location.Y.ToMeters()),
+            Z = Convert.ToSingle(cp.Location.Z.ToMeters())
+          },
+          Direction = new iabi.BCF.APIObjects.V21.Direction
+          {
+            X = Convert.ToSingle(cp.Direction.X),
+            Y = Convert.ToSingle(cp.Direction.Y),
+            Z = Convert.ToSingle(cp.Direction.Z)
+          }
+        
+      })
+      .ToList();
     }
   }
 }
